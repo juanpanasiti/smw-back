353d8b8dacc7837d5f09a0a17793ef86aeae29c0
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-07-11 01:54:22 -0300

    logging and jwt configs

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,3 +1,15 @@
+import logging
+from datetime import datetime
+
 from .config import Settings
 
 settings = Settings()
+
+logging.basicConfig(
+    level=logging.DEBUG if settings.DEBUG else logging.INFO,
+    filemode='a',
+    filename='./logs/{}'.format(
+        datetime.now().strftime('%Y%m%d_save_my_wallet.log')),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,18 @@
 from pydantic_settings import BaseSettings
 
 
-
 class Settings(BaseSettings):
     # API
     PORT: int = 8000
     CONN_DB: str | None = None
 
+    # Config
+    DEBUG: bool = False
+
+    # JWT
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = 'HS256'
+    JWT_EXPIRATION_TIME_MINUTES: int = 3600
 
     class Config:
         env_file = '.env'
