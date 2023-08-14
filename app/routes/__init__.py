e3c5cfc534a6e1dd67903c13ec21f83372b92587
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-08-13 22:09:12 -0300

    redirect root route to /docs

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .v1 import router_v1
+from .root_routes import router as root_router
 
-api_router = APIRouter(prefix='/api')
-api_router.include_router(router_v1)
+api_router = APIRouter()
+api_router.include_router(root_router, include_in_schema=False)
+api_router.include_router(router_v1, prefix='/api')
