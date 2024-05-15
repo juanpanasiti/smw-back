2ded099c1a320a686af716ddbca991ac04628ae8
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-05-15 19:34:22 -0300

    add profile model

---
+++ app/database/models/profile_model.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from . import BaseModel
+
+
+class ProfileModel(BaseModel):
+    __tablename__ = 'profiles'
+
+    first_name: Mapped[str] = mapped_column(
+        String(64), unique=True, nullable=False)
+    last_name: Mapped[str] = mapped_column(
+        String(64), unique=True, nullable=False)
+
+    # PKs
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
