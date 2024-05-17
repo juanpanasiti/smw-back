2636e543bab639c6c1c8f90b6aaab235a0b89fe9
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-05-17 19:41:12 -0300

    update credit card model

--- app/database/models/credit_card_model.py
+++ app/database/models/credit_card_model.py
@@ -1,35 +1,33 @@
 from datetime import date
 from typing import List
 
-from sqlalchemy import String, Float, Integer, ForeignKey, Date
+from sqlalchemy import String, Float, Integer, ForeignKey, Date, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import BaseModel
 from .expense_model import ExpenseModel
 
 
-
-
 class CreditCardModel(BaseModel):
     __tablename__ = 'credit_cards'
 
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    limit: Mapped[int] = mapped_column(Float(precision=2), default=0.0, nullable=False)
-
-    main_credit_card_id: Mapped[int] = mapped_column(Integer, ForeignKey('credit_cards.id'), nullable=True)
+    alias: Mapped[str] = mapped_column(String(100), nullable=False)
+    limit: Mapped[int] = mapped_column(
+        Float(precision=2), default=0.0, nullable=False)
+    main_credit_card_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey('credit_cards.id'), nullable=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
-
-    closing_date: Mapped[date] = mapped_column(Date(), nullable=True)
-    expiring_date: Mapped[date] = mapped_column(Date(), nullable=True)
+    next_closing_date: Mapped[date] = mapped_column(Date(), nullable=True)
+    next_expiring_date: Mapped[date] = mapped_column(Date(), nullable=True)
+    is_enabled: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False)
 
     # Relations
-    expenses: Mapped[List['ExpenseModel']] = relationship()
+    # expenses: Mapped[List['ExpenseModel']] = relationship()
 
     # Calculated fields
-    @property
-    def total_spent(self) -> float:
-        return sum([expense.remaining_amount for expense in self.expenses])
-
+    # @property
+    # def total_spent(self) -> float:
+    #     return sum([expense.remaining_amount for expense in self.expenses])
 
     def __repr__(self) -> str:
         return f'CreditCard {self.name}'
