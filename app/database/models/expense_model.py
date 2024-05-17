886d7f355c1f385833de1dfe23c07d7e6bd1a3ca
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-05-17 19:54:54 -0300

    update expense model

--- app/database/models/expense_model.py
+++ app/database/models/expense_model.py
@@ -1,12 +1,14 @@
 from datetime import date
 from typing import List
 
-from sqlalchemy import String, Date, Float, Integer, Boolean, ForeignKey
+from sqlalchemy import String, Date, Float, Integer, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import BaseModel
 from .payment_model import PaymentModel
 from app.core.enums.payment_status_enum import PaymentStatusEnum
+from app.core.enums.expense_type_enum import ExpenseTypeEnum
+from app.core.enums.expense_status_enum import ExpenseStatusEnum
 
 
 class ExpenseModel(BaseModel):
@@ -14,11 +16,12 @@ class ExpenseModel(BaseModel):
 
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     cc_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    total_amount: Mapped[float] = mapped_column(Float(precision=2), default=0.0, nullable=False)
-    is_subscription: Mapped[bool] = mapped_column(Boolean(), default=False, nullable=False)
-    total_installments: Mapped[int] = mapped_column(Integer(), default=1, nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), default=True, nullable=False)
-    purchased_at: Mapped[date] = mapped_column(Date(), default=date.today(), nullable=False)
+    acquired_at: Mapped[date] = mapped_column(Date(), default=date.today(), nullable=False)
+    amount: Mapped[float] = mapped_column(Float(precision=2), default=0.0, nullable=False)
+    type: Mapped[str] = mapped_column(Enum(ExpenseTypeEnum), default=ExpenseTypeEnum.PURCHASE, nullable=False)
+    installments: Mapped[int] = mapped_column(Integer(), default=1, nullable=False)
+    first_payment_date: Mapped[date] = mapped_column(Date(), nullable=False)
+    status: Mapped[str] = mapped_column(Enum(ExpenseStatusEnum), default=ExpenseStatusEnum.ACTIVE, nullable=False)
 
     # PKs
     credit_card_id: Mapped[int] = mapped_column(Integer, ForeignKey('credit_cards.id'))
@@ -46,10 +49,6 @@ class ExpenseModel(BaseModel):
     @property
     def installments_pending(self) -> int:
         return len([payment for payment in self.payments if payment.status not in [PaymentStatusEnum.PAID, PaymentStatusEnum.CANCELED]])
-    
-    @property
-    def first_payment(self) -> str:
-        return f'{self.payments[0].month}/{self.payments[0].year}' if self.payments else '---'
 
     def __repr__(self) -> str:
         return f'Expense: {self.title}'
