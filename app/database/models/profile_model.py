a138cffbc270f44f118848b063a9ce43628c0b87
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-05-18 23:27:10 -0300

    remove uniq property

--- app/database/models/profile_model.py
+++ app/database/models/profile_model.py
@@ -10,10 +10,8 @@ from . import BaseModel
 class ProfileModel(BaseModel):
     __tablename__ = 'profiles'
 
-    first_name: Mapped[str] = mapped_column(
-        String(64), unique=True, nullable=False)
-    last_name: Mapped[str] = mapped_column(
-        String(64), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(64), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(64), nullable=False)
 
     # PKs
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
