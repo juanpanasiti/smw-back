cc212855e5f3c5c6bb400e8f5d1093a91a200ea0
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-05-20 20:07:47 -0300

    fix credit card schemas

--- app/schemas/credit_card_schemas.py
+++ app/schemas/credit_card_schemas.py
@@ -2,26 +2,24 @@ from datetime import datetime,date
 from pydantic import BaseModel
 
 
-class NewCreditCardReq(BaseModel):
-    name: str
-    limit: int
-
 class CreditCardReq(BaseModel):
-    name: str | None = None
-    limit: int | None = None
-    user_id: int | None = None
-    closing_date: date | None
-    expiring_date: date | None
+    alias: str
+    limit: int
+    main_credit_card_id: int | None = None
+    user_id: int
+    next_closing_date: date | None = None
+    next_expiring_date: date | None = None
+    is_enbled: bool = True
 
 
 class CreditCardRes(BaseModel):
     id: int
-    name: str
+    alias: str
     limit: int
     user_id: int
-    closing_date: date | None
-    expiring_date: date | None
-    total_spent: float
+    next_closing_date: date | None
+    next_expiring_date: date | None
+    # total_spent: float
     created_at: datetime
     updated_at: datetime
 
