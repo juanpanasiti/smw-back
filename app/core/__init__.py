7dfd8e92c9d9b8175a17762dd53c14a078b51b95
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-11-10 02:12:17 -0300

    implement logs backup by day

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,15 +1,30 @@
+import os
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
 from .config import Settings
 
 settings = Settings()
 
-logging.basicConfig(
-    level=logging.DEBUG if settings.DEBUG else logging.INFO,
-    filemode='a',
-    filename='./logs/{}'.format(
-        datetime.now().strftime('%Y%m%d_save_my_wallet.log')),
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+
+log_filename = './logs/save_my_wallet.log'
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG if settings.DEBUG else logging.INFO)
+
+# Handler que rota el archivo de log diariamente
+handler = TimedRotatingFileHandler(
+    log_filename,
+    when='midnight',
+    interval=1,
+    backupCount=0,
 )
+handler.suffix = '%Y%m%d'  # Añade la fecha al final del archivo
+
+# Formato del log
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+
+# Añadir el handler al logger
+logger.addHandler(handler)
