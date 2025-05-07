007d33da03ef39677eadcbc1daeceb54430d8f7c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-05-07 14:54:22 -0300

    minor: undo init

--- app/exceptions/server_exceptions.py
+++ app/exceptions/server_exceptions.py
@@ -4,11 +4,7 @@ from .base_http_exception import BaseHTTPException
 class InternalServerError(BaseHTTPException):
     description = 'Unhadled error.'
     status_code = 500
-    default_message = 'Unhandled error, contact the sysadmin'
     exception_code = 'INTERNAL_SERVER_ERROR'
-
-    def __init__(self, message: str = None) -> None:
-        super().__init__(message or self.default_message)
 
 
 class NotImplemented(BaseHTTPException):
@@ -21,4 +17,3 @@ class ServiceUnavailable(BaseHTTPException):
     description = 'Service is not available.'
     status_code = 503
     exception_code = 'SERVICE_UNAVAILABLE'
-
