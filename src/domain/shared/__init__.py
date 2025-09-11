38985c2f9a574f12a3ac00f442f239acb8eac503
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-11 15:03:55 -0300

    update module exports

--- src/domain/shared/__init__.py
+++ src/domain/shared/__init__.py
@@ -1,7 +1,11 @@
 from .entity_base import EntityBase
+from .entity_factory_base import EntityFactoryBase
 from .value_objects.amount import Amount
+from .helpers import dates as date_helpers
 
 __all__ = [
     'Amount',
     'EntityBase',
+    'EntityFactoryBase',
+    'date_helpers',
 ]
