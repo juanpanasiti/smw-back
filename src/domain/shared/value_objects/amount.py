7ac91ff9f389333835a14c077d66439494bd6c9e
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-11 23:47:20 -0300

    add dunder methods fot substraction, equal and repr

--- src/domain/shared/value_objects/amount.py
+++ src/domain/shared/value_objects/amount.py
@@ -15,3 +15,18 @@ class Amount:
         if not isinstance(other, Amount):
             raise TypeError('Can only add Decimal to Decimal')
         return Amount(self.value + other.value, max(self.precision, other.precision))
+    
+    def __sub__(self, other: 'Amount') -> 'Amount':
+        if not isinstance(other, Amount):
+            raise TypeError('Can only subtract Decimal from Decimal')
+        return Amount(self.value - other.value, max(self.precision, other.precision))
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, (Amount, float, int)):
+            return NotImplemented
+        if isinstance(value, Amount):
+            value = value.value
+        return self.value == float(value)
+    
+    def __repr__(self) -> str:
+        return f'Amount(value={self.value}, precision={self.precision})'
