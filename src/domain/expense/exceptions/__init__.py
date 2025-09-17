bac3142266fea44c0a6c66523864d7d8798d89b4
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-17 00:27:54 -0300

    add exceptions for invalid and not implemented operations

--- src/domain/expense/exceptions/__init__.py
+++ src/domain/expense/exceptions/__init__.py
@@ -1,9 +1,11 @@
-from .expense_exceptions import ExpenseStatusException
+from .expense_exceptions import ExpenseStatusException, ExpenseInvalidOperation, ExpenseNotImplementedOperation
 from .payment_exceptions import PaymentNotFoundInExpenseException
 
 __all__ = [
     # Expense exceptions
     'ExpenseStatusException',
+    'ExpenseInvalidOperation',
+    'ExpenseNotImplementedOperation',
     # Payment exceptions
     'PaymentNotFoundInExpenseException',
 ]
--- src/domain/expense/exceptions/expense_exceptions.py
+++ src/domain/expense/exceptions/expense_exceptions.py
@@ -7,3 +7,20 @@ class ExpenseStatusException(ExceptionBase):
     def __init__(self, message: str):
         super().__init__(message)
         self.code = 'EXPENSE_STATUS_EXCEPTION'
+
+
+class ExpenseInvalidOperation(ExceptionBase):
+    '''Exception raised for invalid expense operations.'''
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.code = 'EXPENSE_INVALID_OPERATION'
+
+
+class ExpenseNotImplementedOperation(ExceptionBase):
+    '''Exception raised for unimplemented expense operations.'''
+    DEFAULT_MESSAGE = 'This operation is not implemented for this expense type.'
+
+    def __init__(self, message: str = DEFAULT_MESSAGE):
+        super().__init__(message)
+        self.code = 'EXPENSE_NOT_IMPLEMENTED_OPERATION'
