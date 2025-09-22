d69ba29984f6c3182b9f85e3953c6be0d2e04b50
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-21 21:55:26 -0300

    export user factory

--- src/domain/auth/__init__.py
+++ src/domain/auth/__init__.py
@@ -1,6 +1,7 @@
 from .preferences import Preferences
 from .profile import Profile
 from .user import User
+from .user_factory import UserFactory
 from .enums.role import Role
 
 
@@ -8,5 +9,6 @@ __all__ = [
     'Preferences',
     'Profile',
     'User',
+    'UserFactory',
     'Role',
 ]
