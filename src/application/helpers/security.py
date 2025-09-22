397e2c2439e37c57d48cd554986ad07942f70719
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-21 21:54:25 -0300

    implement encode and decode methods for jwt

--- src/application/helpers/security.py
+++ src/application/helpers/security.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta, timezone
 import bcrypt
+import jwt
+
+from src.common.exceptions import JWTExpiredError, JWTInvalidError, JWTInvalidSignatureError
 
 
 def hash_password(plain_password: str) -> str:
@@ -9,3 +13,24 @@ def hash_password(plain_password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+def encode_jwt(data: dict, secret: str, algorithm: str, expires_delta: timedelta) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, secret, algorithm=algorithm)
+    return encoded_jwt
+
+
+def decode_jwt(token: str, secret: str, algorithm: str) -> dict:
+    try:
+        payload = jwt.decode(token, secret, algorithms=[algorithm])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise JWTExpiredError()
+    except jwt.InvalidSignatureError:
+        raise JWTInvalidSignatureError()
+    except jwt.InvalidTokenError:
+        raise JWTInvalidError()
