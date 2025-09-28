720dd199148c44d3154b4df4e4de9c79386b9360
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-28 02:45:11 -0300

    fix calling to create method

--- src/application/use_cases/account/credit_card_create_use_case.py
+++ src/application/use_cases/account/credit_card_create_use_case.py
@@ -1,6 +1,9 @@
+from uuid import uuid4
+
 from ...dtos import CreateCreditCardDTO, CreditCardResponseDTO
 from ...ports import CreditCardRepository
 from src.domain.account import CreditCardFactory
+from src.domain.shared import Amount
 
 
 class CreditCardCreateUseCase:
@@ -8,6 +11,17 @@ class CreditCardCreateUseCase:
         self.credit_card_repository = credit_card_repository
 
     def execute(self, credit_card_data: CreateCreditCardDTO) -> CreditCardResponseDTO:
-        credit_card = CreditCardFactory.create(**credit_card_data.model_dump())
+        credit_card = CreditCardFactory.create(
+            id=uuid4(),
+            owner_id=credit_card_data.owner_id,
+            alias=credit_card_data.alias,
+            limit=Amount(credit_card_data.limit),
+            is_enabled=True,
+            main_credit_card_id=credit_card_data.main_credit_card_id,
+            next_closing_date=credit_card_data.next_closing_date,
+            next_expiring_date=credit_card_data.next_expiring_date,
+            financing_limit=Amount(credit_card_data.financing_limit),
+            expenses=[],
+        )
         new_credit_card = self.credit_card_repository.create(credit_card)
         return CreditCardResponseDTO.model_validate(new_credit_card)
