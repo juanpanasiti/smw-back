e3108f1ee370f705c859c9e7eb125fbc3b828171
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-29 18:28:56 -0300

    fix response dto issue

--- src/application/use_cases/expense/expense_category_update_use_case.py
+++ src/application/use_cases/expense/expense_category_update_use_case.py
@@ -16,4 +16,10 @@ class ExpenseCategoryUpdateUseCase:
         for field, value in category_data.model_dump(exclude_unset=True).items():
             setattr(expense_category, field, value)
         updated_expense_category = self.expense_category_repository.update(expense_category)
-        return ExpenseCategoryResponseDTO.model_validate(updated_expense_category)
+        return ExpenseCategoryResponseDTO(
+            id=updated_expense_category.id,
+            owner_id=updated_expense_category.owner_id,
+            name=updated_expense_category.name,
+            description=updated_expense_category.description,
+            is_income=updated_expense_category.is_income,
+        )
