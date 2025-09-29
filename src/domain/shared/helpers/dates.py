96fc3610366aa1714853188426b6df14912e6e55
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-09-29 09:44:14 -0300

    fix adding months calculation

--- src/domain/shared/helpers/dates.py
+++ src/domain/shared/helpers/dates.py
@@ -12,6 +12,7 @@ def calc_days_until(date_to: date) -> int:
     delta = date_to - today
     return delta.days if delta.days >= 0 else 0
 
+
 def add_months_to_date(start_date: date, months: int) -> date:
     '''
     Add a specified number of months to a given date.
@@ -21,7 +22,20 @@ def add_months_to_date(start_date: date, months: int) -> date:
     :return: The new date after adding the specified number of months.
     '''
     month = start_date.month - 1 + months
+    day = start_date.day
     year = start_date.year + month // 12
-    month = month % 12 + 1
-    day = min(start_date.day, (date(year, month, 1) - timedelta(days=1)).day)
-    return date(year, month, day)
+    try:
+        new_date = date(year, month % 12 + 1, day)
+    except ValueError as ex:
+        new_date = start_date + timedelta(days=30)
+    return new_date
+
+
+def is_leap_year(year: int) -> bool:
+    '''
+    Check if a given year is a leap year.
+
+    :param year: The year to check.
+    :return: True if the year is a leap year, False otherwise.
+    '''
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
