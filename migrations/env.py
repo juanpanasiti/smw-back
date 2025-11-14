9d957e25f4437b3141630aed4c0502dc71c414ef
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-11-14 19:22:38 -0300

    clean and update migrations

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from app.core import settings
-from app.database.models import base_model
+from src.config import settings
+from src.infrastructure.database.models import base_model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
