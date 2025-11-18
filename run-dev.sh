014b9aa586eeaadc7b5cb7e2836abe4938b23a63
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-11-18 20:39:45 -0300

    update run dev script

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,3 @@
-pip install -r requirements-dev.txt
+# pip install -r requirements-dev.txt
 docker compose up smw-db -d
-uvicorn "src.entrypoints.api:app" --host 0.0.0.0 --port 8000 --reload
+fastapi dev src/entrypoints/api.py --host 0.0.0.0 --port 8000
